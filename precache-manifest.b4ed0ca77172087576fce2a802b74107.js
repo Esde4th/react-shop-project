0017144182912890a469602337ca37b6a301e782
Bambietta <bambi666@example.com>
2022-02-05 00:00:55 +0200

    Updates

---
+++ precache-manifest.b4ed0ca77172087576fce2a802b74107.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4a6417817538152d39c3503a5318dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "402372f7f901117bbcf9",
+    "url": "/static/css/main.34e681ba.chunk.css"
+  },
+  {
+    "revision": "b974e96455baf8552a88",
+    "url": "/static/js/2.5058aa63.chunk.js"
+  },
+  {
+    "revision": "402372f7f901117bbcf9",
+    "url": "/static/js/main.1dbe658d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
